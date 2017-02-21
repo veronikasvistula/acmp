535a73ec24e6958659eb07788c835e9b612b4c7c
veronikasvistula <veronika.svistula@gmail.com>
2017-02-21 18:18:13 +0200

    Add files via upload

---
+++ 0149 разворот.py
@@ -0,0 +1,21 @@
+import sys
+ 
+N, = [int(s) for s in sys.stdin.readline().strip().split()]
+lst = [int(s) for s in sys.stdin.readline().strip().split()]
+
+middle = N // 2
+
+i_start = 0
+i_finish = N - 1
+while i_start < middle:
+    lst[i_start], lst[i_finish] = lst[i_finish], lst[i_start]
+    i_start += 1
+    i_finish -= 1
+
+p = 0
+while p < N:
+    lst[p] = str(lst[p])
+    p += 1
+
+
+print (' '.join(lst))
